1b2ece03671ba292ea22104641d55e3ceecca526
iferas93 <ferasmo7mad@gmail,cin>
2022-10-06 23:16:58 +0300

    wip:classes and objects

--- classes-objects.php
+++ classes-objects.php
@@ -4,10 +4,12 @@
  * Initialize class
  */
 class Person{
-    public $name;
-    private $age;
-    private $id;
+    public $name="Naya";
+    private $age=18;
+    private $id="123123";
 }
 
 // Initialize object
 $person_1= new Person();
+$person_1->name="Naya Feras";
+var_dump($person_1);
